1089d989c991f5c7cc561d8dbc56034936c2c59b
Poshak Jaiswal <poshak@greytip.com>
2021-02-23 01:52:48 +0530

    Adding Algorithm and test classes

---
+++ Algorithm_preparation/Graph_Algos/graph_algos/graph_search.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+
+class GraphSearch:
+    debug_flag = False
+    graph_to_search = {}
+
+    def __init__(self, given_graph_to_search):
+        setattr(self, 'graph_to_search', given_graph_to_search)
+        pass
+
+    @staticmethod
+    def print_helper(persons_already_searched, queue_to_put_people_for_searching):
+        print("----------------")
+        print(" person already searched")
+        print(persons_already_searched)
+        print("----------------")
+        print(" queue_to_put_people_for_searching")
+        print(queue_to_put_people_for_searching)
+        print("----------------")
+
+        return
+
+    def search_using_bfs(self, person_to_search="jonny"):
+        search_queue = deque()
+        search_queue += self.graph_to_search["you"]  # when you are given a start position
+        already_searched = []
+        # key, search_queue = graph.popitem()
+        while search_queue:  # if the queue is not empty
+            current_person = search_queue.popleft()
+            if current_person not in already_searched:
+                if current_person == person_to_search:
+                    return "Yes you Found " + current_person
+                else:
+                    search_queue += self.graph_to_search[current_person]
+                    already_searched.append(current_person)
+
+        return "No we didnot find who you were looking in your connections"
+
+    def breadth_first_search(self, person_to_search="thom"):
+        persons_already_searched = []
+        queue_to_put_people_for_searching = []
+        queue_to_put_people_for_searching += self.graph_to_search["you"]
+        if self.debug_flag:
+            self.print_helper(persons_already_searched, queue_to_put_people_for_searching)
+
+        while queue_to_put_people_for_searching:  # check the queue is already not empty
+
+            if self.debug_flag:
+                self.print_helper(persons_already_searched, queue_to_put_people_for_searching)
+            current_person = queue_to_put_people_for_searching.pop(0)  # getting the first item from the queue
+
+            if self.debug_flag:
+                print("current person -->> " + current_person)
+
+            if current_person not in persons_already_searched:
+
+                if current_person == person_to_search:
+                    return "Yes you Found " + current_person
+                else:
+                    # See that append adds a single element to the list, which may be anything. +=[] joins the lists.
+                    '''
+                    >>> a=[]
+                    >>> a.append([1,2])
+                    >>> a
+                    [[1, 2]]
+                    >>> a=[]
+                    >>> a+=[1,2]
+                    >>> a
+                    [1, 2]
+                    '''
+                    queue_to_put_people_for_searching += (self.graph_to_search[current_person])
+                    persons_already_searched.append(current_person)
+
+        return False
---
+++ Algorithm_preparation/graph_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+from Graph_Algos import graph_algos
+
+
+class GraphTestCases(unittest.TestCase):
+    def test_person_present_in_graph(self):
+        graph_instance = graph_algos.GraphSearch(graph)
+        search_result = graph_instance.breadth_first_search()
+        self.assertEqual(True, True)
+
+    def test_person_absent_in_graph(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
